d2ad4c69ec06d48d06c016c7b3d9402d0772dc6a
Ujjawal Singh <147547611+its-ujjawal@users.noreply.github.com>
2023-10-12 22:30:26 +0530

    day 3
    Age of a person after 100 year

---
+++ age after 100 years.py
@@ -0,0 +1,6 @@
+name=input("enter your name:")
+age=int(input("enter your age:"))
+current_year=2023
+year_to_100=100-age
+year_turn_100=current_year+year_to_100
+print(f"hello,{name}!you turn to 100 year in the age {year_turn_100}")
