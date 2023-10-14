8276f9819cb4ccb86b1aa6500b46ae7be9262106
Ujjawal Singh <147547611+its-ujjawal@users.noreply.github.com>
2023-10-14 22:21:19 +0530

    Day 4
    - Swapping of two numbers
    - Age of a person after 100 years

---
+++ swapping of two number.py
@@ -0,0 +1,7 @@
+num1=int(input("enter the number:"))
+num2=int(input("enter the number:"))
+print("original number:num1=",num1,"num2=",num2)
+temp=num1
+num1=num2
+num2=temp
+print("swapped number:num1=",num1,"num2=",num2)
